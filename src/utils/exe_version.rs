48803029106c85439828ab20dd5a7cb2195f312d
L0serj3rry <dsyourshy@qq.com>
2023-02-12 17:43:14 +0800

    feat:增加对32位程序的版本号读取

--- src/utils/exe_version.rs
+++ src/utils/exe_version.rs
@@ -1,12 +1,13 @@
 use anyhow::{anyhow, Result};
-use pelite::pe64::{Pe, PeFile};
+use pelite::{pe64,pe32};
 use pelite::FileMap;
 use std::path::Path;
 
-pub fn get_exe_version<P: AsRef<Path>>(file_path: P) -> Result<String> {
+fn get_64_version<P: AsRef<Path>>(file_path: P) -> Result<String> {
+    use pelite::pe::Pe;
     let path = file_path.as_ref();
     if let Ok(map) = FileMap::open(path) {
-        let file = PeFile::from_bytes(&map).map_err(|e| {
+        let file = pe64::PeFile::from_bytes(&map).map_err(|e| {
             anyhow!(
                 "Error:Can't read pe information of '{:?}' : {}",
                 &path,
@@ -43,11 +44,62 @@ pub fn get_exe_version<P: AsRef<Path>>(file_path: P) -> Result<String> {
     }
 }
 
+fn get_32_version<P: AsRef<Path>>(file_path: P) -> Result<String> {
+    use pelite::pe32::Pe;
+    let path = file_path.as_ref();
+    if let Ok(map) = FileMap::open(path) {
+        let file = pe32::PeFile::from_bytes(&map).map_err(|e| {
+            anyhow!(
+                "Error:Can't read pe information of '{:?}' : {}",
+                &path,
+                e.to_string()
+            )
+        })?;
+
+        let resources = file.resources().map_err(|e| {
+            anyhow!(
+                "Error:Can't read resources of '{:?}' : {}",
+                &path,
+                e.to_string()
+            )
+        })?;
+        let version_info = resources.version_info().map_err(|e| {
+            anyhow!(
+                "Error:Can't read version information of '{:?}' : {}",
+                &path,
+                e.to_string()
+            )
+        })?;
+        let fix_opt = version_info.fixed();
+        if fix_opt.is_none() {
+            return Err(anyhow!("Error:Can't get version info of '{:?}'", &path));
+        }
+        let file_version = fix_opt.unwrap().dwFileVersion;
+
+        Ok(format!(
+            "{}.{}.{}.{}",
+            file_version.Major, file_version.Minor, file_version.Patch, file_version.Build
+        ))
+    } else {
+        Err(anyhow!("Error:Can't read version of '{:?}'", &path))
+    }
+}
+
+pub fn get_exe_version<P: AsRef<Path>>(file_path: P)->Result<String>{
+    let try_get=get_32_version(&file_path);
+    let res=if try_get.is_ok(){
+        try_get.unwrap()
+    }else{
+        get_64_version(file_path)?
+    };
+    Ok(res)
+}
+
 #[test]
 fn test_get_exe_version() {
     let res = get_exe_version(r"D:\CnoRPS\Dism++10.1.1002.1\Dism++x64.exe".to_string()).unwrap();
     assert_eq!(res, String::from("10.1.1002.1"));
 
-    let res = get_exe_version(r"D:\CnoRPS\aria2\aria2c.exe".to_string());
-    println!("{:?}", res);
+    let res = get_exe_version(r"D:\CnoRPS\Dism++10.1.1002.1\Dism++x86.exe".to_string()).unwrap();
+    assert_eq!(res, String::from("10.1.1002.1"));
 }
