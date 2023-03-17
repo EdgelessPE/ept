e9dc0ee353062d68bb2c70282b5412ee10905e98
L0serj3rry <dsyourshy@qq.com>
2023-03-17 22:22:43 +0800

    feat:完善进程控制

--- src/utils/process.rs
+++ src/utils/process.rs
@@ -1,8 +1,34 @@
-use std::process::Command;
+use sysinfo::{ProcessExt, System, SystemExt};
 
 pub fn kill_with_name(name: String) -> bool {
-    let cmd = format!("TASKKILL /F /IM {} /T", &name);
-    let output = Command::new("cmd").args(["/C", &cmd]).output();
+    let s = System::new_all();
+    let mut res=true;
+    for process in s.processes_by_exact_name(&name) {
+        if !process.kill(){
+            res=false;
+        }
+    }
+    res
+}
 
-    output.is_ok()
+pub fn is_alive_with_name(name:String) -> bool {
+    let s = System::new_all();
+    let mut processes=s.processes_by_exact_name(&name);
+    processes.next().is_some()
+}
+
+
+#[test]
+fn test_kill_with_name(){
+    let res=kill_with_name("哔哩哔哩.exe".to_string());
+    assert!(res);
+}
+
+#[test]
+fn test_is_alive_with_name(){
+    let res=is_alive_with_name("Code.exe".to_string());
+    assert!(res);
+    
+    let res=is_alive_with_name("code.exe".to_string());
+    assert_eq!(res,false);
 }
