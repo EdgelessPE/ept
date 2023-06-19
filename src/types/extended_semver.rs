932fe9bc8dcd88a4ed53c7c9af954fc82883452b
Cno <dsyourshy@qq.com>
2023-06-19 16:41:42 +0800

    feat:添加semver官网比较案例

--- src/types/extended_semver.rs
+++ src/types/extended_semver.rs
@@ -4,32 +4,59 @@ use std::cmp::Ordering::{Equal, Greater, Less};
 use std::fmt;
 use std::{cmp::Ordering, str::FromStr};
 
-fn split_pre_build(raw:&String)->(String,String){
+fn split_pre_build(raw: &String) -> (String, String) {
     // 分割 -
-    let sp:Vec<&str>=raw.split("-").collect();
-    let clear=sp[0].to_string();
+    let sp: Vec<&str> = raw.split("-").collect();
+    let clear = sp[0].to_string();
 
     // 分割 +
-    let sp:Vec<&str>=clear.split("+").collect();
-    let clear=sp[0].to_string();
+    let sp: Vec<&str> = clear.split("+").collect();
+    let clear = sp[0].to_string();
 
     // 根据 clear 长度拆出 pre 和 build 部分
-    let pre_build=&raw[clear.len()..];
+    let pre_build = &raw[clear.len()..];
 
-    (clear,pre_build.to_string())
+    (clear, pre_build.to_string())
 }
 
 #[test]
-fn test_split_pre_build(){
-    assert_eq!(split_pre_build(&"1.0.0.0".to_string()),("1.0.0.0".to_string(),"".to_string()));
-    assert_eq!(split_pre_build(&"1.0.0.0-alpha".to_string()),("1.0.0.0".to_string(),"-alpha".to_string()));
-    assert_eq!(split_pre_build(&"1.2.0.0-alpha.1".to_string()),("1.2.0.0".to_string(),"-alpha.1".to_string()));
-    assert_eq!(split_pre_build(&"1.0.0.0-alpha.beta".to_string()),("1.0.0.0".to_string(),"-alpha.beta".to_string()));
-    assert_eq!(split_pre_build(&"1.0.0.0-beta".to_string()),("1.0.0.0".to_string(),"-beta".to_string()));
-    assert_eq!(split_pre_build(&"1.0.0.0-beta.2".to_string()),("1.0.0.0".to_string(),"-beta.2".to_string()));
-    assert_eq!(split_pre_build(&"1.0.0.0-beta.11+build114514".to_string()),("1.0.0.0".to_string(),"-beta.11+build114514".to_string()));
-    assert_eq!(split_pre_build(&"1.0.0.0-rc.1".to_string()),("1.0.0.0".to_string(),"-rc.1".to_string()));
-    assert_eq!(split_pre_build(&"1.0.0.0+BLAKE1919810".to_string()),("1.0.0.0".to_string(),"+BLAKE1919810".to_string()));
+fn test_split_pre_build() {
+    assert_eq!(
+        split_pre_build(&"1.0.0.0".to_string()),
+        ("1.0.0.0".to_string(), "".to_string())
+    );
+    assert_eq!(
+        split_pre_build(&"1.0.0.0-alpha".to_string()),
+        ("1.0.0.0".to_string(), "-alpha".to_string())
+    );
+    assert_eq!(
+        split_pre_build(&"1.2.0.0-alpha.1".to_string()),
+        ("1.2.0.0".to_string(), "-alpha.1".to_string())
+    );
+    assert_eq!(
+        split_pre_build(&"1.0.0.0-alpha.beta".to_string()),
+        ("1.0.0.0".to_string(), "-alpha.beta".to_string())
+    );
+    assert_eq!(
+        split_pre_build(&"1.0.0.0-beta".to_string()),
+        ("1.0.0.0".to_string(), "-beta".to_string())
+    );
+    assert_eq!(
+        split_pre_build(&"1.0.0.0-beta.2".to_string()),
+        ("1.0.0.0".to_string(), "-beta.2".to_string())
+    );
+    assert_eq!(
+        split_pre_build(&"1.0.0.0-beta.11+build114514".to_string()),
+        ("1.0.0.0".to_string(), "-beta.11+build114514".to_string())
+    );
+    assert_eq!(
+        split_pre_build(&"1.0.0.0-rc.1".to_string()),
+        ("1.0.0.0".to_string(), "-rc.1".to_string())
+    );
+    assert_eq!(
+        split_pre_build(&"1.0.0.0+BLAKE1919810".to_string()),
+        ("1.0.0.0".to_string(), "+BLAKE1919810".to_string())
+    );
 }
 
 /// 拓展的 SemVer 规范，在修订号后面多了一位保留号（reserved）用于标记不同的打包版本
@@ -46,14 +73,21 @@ pub struct ExSemVer {
 }
 
 impl ExSemVer {
-    pub fn _new(major: u64, minor: u64, patch: u64, reserved: u64, pre:Prerelease,build:BuildMetadata) -> Self {
+    pub fn _new(
+        major: u64,
+        minor: u64,
+        patch: u64,
+        reserved: u64,
+        pre: Prerelease,
+        build: BuildMetadata,
+    ) -> Self {
         ExSemVer {
             major,
             minor,
             patch,
             reserved,
-            pre:pre.clone(),
-            build:build.clone(),
+            pre: pre.clone(),
+            build: build.clone(),
             semver_instance: semver::Version {
                 major,
                 minor,
@@ -65,7 +99,7 @@ impl ExSemVer {
     }
     pub fn parse(text: &String) -> Result<Self> {
         // 分割 pre 和 build
-        let (clear_text,pre_build)=split_pre_build(text);
+        let (clear_text, pre_build) = split_pre_build(text);
 
         // 使用小数点分割
         let s: Vec<&str> = clear_text.split(".").collect();
@@ -77,9 +111,9 @@ impl ExSemVer {
         }
 
         // 生成标准 semver
-        let sem_version = format!("{}.{}.{}{}", s[0], s[1], s[2],pre_build);
+        let sem_version = format!("{}.{}.{}{}", s[0], s[1], s[2], pre_build);
         let semver = semver::Version::parse(&sem_version)?;
-        let semver_instance=semver.clone();
+        let semver_instance = semver.clone();
 
         // 解析字符串为 u64
         let major = s[0].parse::<u64>()?;
@@ -92,8 +126,8 @@ impl ExSemVer {
             minor,
             patch,
             reserved,
-            pre:semver.pre,
-            build:semver.build,
+            pre: semver.pre,
+            build: semver.build,
             semver_instance,
         })
     }
@@ -104,7 +138,7 @@ impl ExSemVer {
 
 impl From<semver::Version> for ExSemVer {
     fn from(sv: semver::Version) -> Self {
-        let semver_instance=sv.clone();
+        let semver_instance = sv.clone();
         Self {
             major: sv.major,
             minor: sv.minor,
@@ -210,11 +244,11 @@ impl fmt::Display for ExSemVer {
 #[test]
 fn test_ex_semver() {
     // 不带 pre 和 build
-    let v1 = ExSemVer::_new(1, 2, 3, 4,Prerelease::EMPTY,BuildMetadata::EMPTY);
+    let v1 = ExSemVer::_new(1, 2, 3, 4, Prerelease::EMPTY, BuildMetadata::EMPTY);
     let v2 = ExSemVer::from_str("1.2.3.4").unwrap();
     assert_eq!(v1, v2);
     assert_eq!(v1.to_string(), String::from("1.2.3.4"));
-    
+
     assert!(ExSemVer::parse(&"1.12.3".to_string()).is_err());
 
     let v1 = ExSemVer::parse(&"1.2.3.4".to_string()).unwrap();
@@ -242,20 +276,69 @@ fn test_ex_semver() {
     assert_eq!(v2, v3);
 
     // 带 pre 和 build
-    let v1=ExSemVer::parse(&"1.2.3.4-alpha.2.turing".to_string()).unwrap();
-    assert_eq!(v1,ExSemVer::_new(1, 2, 3, 4, Prerelease::from_str("alpha.2.turing").unwrap(), BuildMetadata::EMPTY));
+    let v1 = ExSemVer::parse(&"1.2.3.4-alpha.2.turing".to_string()).unwrap();
+    assert_eq!(
+        v1,
+        ExSemVer::_new(
+            1,
+            2,
+            3,
+            4,
+            Prerelease::from_str("alpha.2.turing").unwrap(),
+            BuildMetadata::EMPTY
+        )
+    );
 
-    let v2=ExSemVer::parse(&"1.20.3.4+build114514".to_string()).unwrap();
-    assert_eq!(v2,ExSemVer::_new(1, 20, 3, 4, Prerelease::EMPTY, BuildMetadata::from_str("build114514").unwrap()));
+    let v2 = ExSemVer::parse(&"1.20.3.4+build114514".to_string()).unwrap();
+    assert_eq!(
+        v2,
+        ExSemVer::_new(
+            1,
+            20,
+            3,
+            4,
+            Prerelease::EMPTY,
+            BuildMetadata::from_str("build114514").unwrap()
+        )
+    );
 
-    let v3=ExSemVer::parse(&"1.12.3.4-beta.2.edgeless+blake456".to_string()).unwrap();
-    assert_eq!(v3,ExSemVer::_new(1, 12, 3, 4, Prerelease::from_str("beta.2.edgeless").unwrap(), BuildMetadata::from_str("blake456").unwrap()));
+    let v3 = ExSemVer::parse(&"1.12.3.4-beta.2.edgeless+blake456".to_string()).unwrap();
+    assert_eq!(
+        v3,
+        ExSemVer::_new(
+            1,
+            12,
+            3,
+            4,
+            Prerelease::from_str("beta.2.edgeless").unwrap(),
+            BuildMetadata::from_str("blake456").unwrap()
+        )
+    );
 
-    assert!(v1<v3);
-    assert!(v1<v2);
-    assert!(v2>v3);
+    assert!(v1 < v3);
+    assert!(v1 < v2);
+    assert!(v2 > v3);
 
     assert!(ExSemVer::parse(&"1.12.3-alpha".to_string()).is_err());
     assert!(ExSemVer::parse(&"1.12.3-alpha-beta".to_string()).is_err());
     assert!(ExSemVer::parse(&"1.12.3+alpha-beta".to_string()).is_err());
+
+    // 测试 pre 与 build比较关系
+    let arr: Vec<ExSemVer> = vec![
+        "1.0.0.0-alpha",
+        "1.0.0.0-alpha.1",
+        "1.0.0.0-alpha.beta",
+        "1.0.0.0-beta",
+        "1.0.0.0-beta.2",
+        "1.0.0.0-beta.11",
+        "1.0.0.0-rc.1",
+        "1.0.0.0",
+    ]
+    .into_iter()
+    .map(|str| ExSemVer::from_str(str).unwrap())
+    .collect();
+
+    for i in 0..arr.len() - 1 {
+        assert!(arr[i] < arr[i + 1]);
+    }
 }
