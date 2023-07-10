f47626969648c3854bcc48fac082513500a3200a
L0serj3rry <dsyourshy@qq.com>
2023-07-10 22:07:14 +0800

    feat:meta入口支持本地安装的包名称

--- src/entrances/meta.rs
+++ src/entrances/meta.rs
@@ -1,24 +1,45 @@
-use std::collections::{HashMap, HashSet};
+use std::{
+    collections::{HashMap, HashSet},
+    path::{Path, PathBuf},
+};
 
 use crate::{
     p2s,
     parsers::parse_workflow,
     types::{
         meta::MetaResult,
+        package::GlobalPackage,
         permissions::{Generalizable, Permission, PermissionLevel},
     },
+    utils::{find_scope_with_name_locally, get_path_apps},
 };
-use anyhow::Result;
+use anyhow::{anyhow, Result};
 
-use super::{utils::package::unpack_nep, verify::verify};
+use super::{info_local, utils::package::unpack_nep};
 
-pub fn meta(source_file: &String, verify_signature: bool) -> Result<MetaResult> {
+fn find_meta_target(input: &String, verify_signature: bool) -> Result<(PathBuf, GlobalPackage)> {
+    // 作为路径使用，可以是一个包或者已经解包的目录
+    let p = Path::new(input);
+    if p.exists() {
+        return unpack_nep(input, verify_signature);
+    }
+
+    // 作为名称使用，在本地已安装列表中搜索
+    if let Ok(scope) = find_scope_with_name_locally(input) {
+        let path = get_path_apps(&scope, input, false)?;
+        let (pkg, _) = info_local(&scope, input)?;
+        return Ok((path, pkg));
+    }
+
+    Err(anyhow!(
+        "Error:Failed to find meta by '{input}', input valid path or installed package name"
+    ))
+}
+
+pub fn meta(input: &String, verify_signature: bool) -> Result<MetaResult> {
     // 解包
-    let (temp_dir_inner_path, global) = unpack_nep(source_file, verify_signature)?;
+    let (temp_dir_inner_path, global) = find_meta_target(input, verify_signature)?;
     let temp_dir = p2s!(temp_dir_inner_path);
-
-    // 校验
-    verify(&temp_dir)?;
 
     // 检查工作流存在
     let exists_workflows: Vec<(String, String)> = vec!["setup.toml", "update.toml", "remove.toml"]
