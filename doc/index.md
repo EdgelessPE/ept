f71d0ca931438124980ce8f8c0fc67b5a207f0b5
Cno <dongheming@bytedance.com>
2023-09-01 20:42:51 +0800

    doc:完成文档基础结构

--- doc/index.md
+++ doc/index.md
@@ -1,1 +1,26 @@
-# Hello World
+---
+pageType: home
+
+hero:
+  name: ept
+  text: 基于 Nep 规范打造
+  tagline: 新一代 Windows 包管理解决方案
+  actions:
+    - theme: brand
+      text: 快速开始
+      link: /ept/quick-start
+    - theme: alt
+      text: 介绍
+      link: /nep/introduction/1-whats
+
+features:
+  - title: '使用 Rust 构建'
+    details: 编译后的二进制文件仅4MB左右大小，无运行时且无需依赖任何运行库或动态链接库。
+    icon: 🚀
+  - title: '强大而安全'
+    details: 摘要签名使用完备且快速的 blake3 + ed25519 方案，同时享受工作流步骤提供的校验、装箱单、权限控制能力。
+    icon: 👊
+  - title: '高效压缩'
+    details: 使用 Z-Standard 压缩算法提供无与伦比的压缩率与解压速度。
+    icon: 📦
+---
--- rspress.config.ts
+++ rspress.config.ts
@@ -3,4 +3,28 @@ import { defineConfig } from 'rspress/config';
 
 export default defineConfig({
   root: path.join(__dirname, 'doc'),
+  // base:'ept',
+  title:'ept',
+  description:'Modern package solution for Windows',
+  icon:'https://home.edgeless.top/favicon.ico',
+  themeConfig: {
+    footer:{
+      message:'MPL-2.0 Licensed | Rendered by Rspress',
+    },
+    socialLinks: [
+      {
+        icon: 'github',
+        mode:'link',
+        content: 'https://github.com/EdgelessPE/ept',
+      },
+    ],
+    editLink: {
+      docRepoBaseUrl:
+        'https://github.com/EdgelessPE/ept/tree/develop/doc',
+      text: 'Edit this page on GitHub',
+    },
+  },
+  markdown:{
+    checkDeadLinks: true,
+  },
 });
