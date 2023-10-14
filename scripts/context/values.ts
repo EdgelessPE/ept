852e84c775e296eb0c2191957595b7a099dd521a
Cno <dsyourshy@qq.com>
2023-10-14 18:52:42 +0800

    fix: 添加递归标记并修复问题

--- scripts/context/values.ts
+++ scripts/context/values.ts
@@ -1,23 +1,23 @@
-import { PermissionLevel } from "../type";
-import { splitBlock } from "../block";
+import { type PermissionLevel } from '../type'
+import { splitBlock } from '../block'
 
-export function parseInnerValues(file: string):{name:string,level:PermissionLevel,wiki?:string,demo?:string}[] {
-  const splittedBlock = splitBlock({ file, startsWith: "define_values!" });
+export function parseInnerValues (file: string): Array<{ name: string, level: PermissionLevel, wiki?: string, demo?: string }> {
+  const splittedBlock = splitBlock({ file, startsWith: 'define_values!' })
   return splittedBlock.map(({ wiki, declaration, demo }) => {
     const m = declaration.match(
       /\{"\$\{(\w+)\}",[\w.()]+\(\),PermissionLevel::(\w+)\},?/
-    );
+    )
     if (m) {
       return {
         name: m[1],
         level: m[2] as PermissionLevel,
         wiki,
-        demo,
-      };
+        demo
+      }
     } else {
       throw new Error(
         `Error:Failed to parse value declaration line '${declaration}'`
-      );
+      )
     }
-  });
+  })
 }
