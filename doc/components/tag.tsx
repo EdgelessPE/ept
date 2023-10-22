9246322aba7c6b537ff4a0b079a1cf81ca0af5c6
Cno <dsyourshy@qq.com>
2023-10-22 22:49:15 +0800

    feat: 分离tag样式

--- doc/components/tag.tsx
+++ doc/components/tag.tsx
@@ -1,12 +1,6 @@
-import { ReactNode } from "react"
+import type { ReactNode } from "react";
+import "./index.css";
 
-export const Tag=({children}:{children:ReactNode})=>{
-  return <span style={{
-    display:'inline-block',
-    border:'1px solid transparent',
-    borderRadius: 10,
-    color:'var(--rp-c-brand-dark)',
-    background:'#eaf3fe',
-    padding:'0 8px'
-  }}>{children}</span>
-}
+export const Tag = ({ children }: { children: ReactNode }) => {
+  return <span className="tag">{children}</span>;
+};
