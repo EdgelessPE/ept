ef69ae41c1e90e2cfefa6282ceb7c3e5e029c0da
Cno <dsyourshy@qq.com>
2024-05-03 01:09:28 +0800

    chore: 删除百分比计算

--- src/utils/download.rs
+++ src/utils/download.rs
@@ -1,6 +1,7 @@
 use anyhow::Result;
 use indicatif::{ProgressBar, ProgressState, ProgressStyle};
 use reqwest::blocking::Client;
+use std::cmp::min;
 use std::fs::File;
 use std::io::{Read, Write};
 
@@ -8,7 +9,7 @@ pub fn download(url: &String, at: &String) -> Result<()> {
     log!("Info:Start downloading '{url}'");
     // 创建进度条
     let pb = ProgressBar::new(0);
-    pb.set_style(ProgressStyle::with_template("{spinner:.green} [{elapsed_precise}] [{wide_bar:.cyan/blue}] {bytes}/{total_bytes} ({bytes_per_sec}, {eta})")
+    pb.set_style(ProgressStyle::with_template("{spinner:.green} [{elapsed_precise}] [{wide_bar:.cyan/blue}] {bytes}/{total_bytes} ({eta})")
         .unwrap()
         .with_key("eta", |state: &ProgressState, w: &mut dyn std::fmt::Write| write!(w, "{:.1}s", state.eta().as_secs_f64()).unwrap())
         .progress_chars("#>-"));
@@ -25,13 +26,16 @@ pub fn download(url: &String, at: &String) -> Result<()> {
     let mut file = File::create(at)?;
 
     let mut buf = vec![0; 1024];
+    let mut downloaded = 0;
     while let Ok(n) = response.read(&mut buf) {
         if n == 0 {
             break;
         }
 
         // 更新进度条
-        pb.set_position(pb.position() + n as u64);
+        let new = min(downloaded + n as u64, content_length);
+        downloaded = new;
+        pb.set_position(new);
 
         // 写入文件
         file.write_all(&buf[0..n])?;
@@ -46,9 +50,8 @@ pub fn download(url: &String, at: &String) -> Result<()> {
 // #[test]
 // fn test_download() {
 //     download(
-//         &"http:/localhost:3000/api/redirect?path=/nep/Microsoft/VSCode/VSCode_1.85.1.0_Cno.nep"
-//             .to_string(),
-//         &"down_test.nep".to_string(),
+//         &"http:/localhost:3000/api/redirect?path=/Socket/Edgeless_Beta_4.1.0.iso".to_string(),
+//         &"down_test.iso".to_string(),
 //     )
 //     .unwrap();
 // }
