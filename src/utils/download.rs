ef9300a874980bf36bb81994498fc08c68702598
Cno <dsyourshy@qq.com>
2024-05-03 00:52:19 +0800

    feat: 优化下载进度条

--- src/utils/download.rs
+++ src/utils/download.rs
@@ -1,50 +1,54 @@
 use anyhow::Result;
+use indicatif::{ProgressBar, ProgressState, ProgressStyle};
 use reqwest::blocking::Client;
 use std::fs::File;
 use std::io::{Read, Write};
 
 pub fn download(url: &String, at: &String) -> Result<()> {
-    let mut response = Client::new().get(url).send()?;
+    log!("Info:Start downloading '{url}'");
+    // 创建进度条
+    let pb = ProgressBar::new(0);
+    pb.set_style(ProgressStyle::with_template("{spinner:.green} [{elapsed_precise}] [{wide_bar:.cyan/blue}] {bytes}/{total_bytes} ({eta})")
+        .unwrap()
+        .with_key("eta", |state: &ProgressState, w: &mut dyn std::fmt::Write| write!(w, "{:.1}s", state.eta().as_secs_f64()).unwrap())
+        .progress_chars("#>-"));
+    let client = Client::new();
 
-    let total_size = response.content_length().unwrap_or(0);
+    // 发送 GET 请求
+    let mut response = client.get(url).send()?;
 
-    let mut position = 0;
-    let mut buffer = [0u8; 1024];
+    // 尝试获取内容长度
+    let content_length = response.content_length().unwrap_or(0);
+    pb.set_length(content_length);
 
+    // 创建文件以写入数据
     let mut file = File::create(at)?;
 
-    while let Ok(length) = response.read(&mut buffer) {
-        if length == 0 {
+    let mut buf = vec![0; 1024];
+    while let Ok(n) = response.read(&mut buf) {
+        if n == 0 {
             break;
         }
 
-        file.write_all(&buffer[..length])?;
+        // 更新进度条
+        pb.set_position(pb.position() + n as u64);
 
-        // 更新已处理的数据量
-        position += length as u64;
-
-        // 打印下载进度
-        println!(
-            "Downloaded {} of {}, {:.2}%",
-            position,
-            total_size,
-            if total_size > 0 {
-                (100 * position / total_size) as usize
-            } else {
-                0
-            }
-        );
+        // 写入文件
+        file.write_all(&buf[0..n])?;
     }
+    // 下载完成，清除进度条
+    pb.finish_and_clear();
+    log!("Info:Downloaded file stored at '{at}'");
 
     Ok(())
 }
 
-#[test]
-fn test_download() {
-    download(
-        &"http:/localhost:3000/api/redirect?path=/nep/Microsoft/VSCode/VSCode_1.85.1.0_Cno.nep"
-            .to_string(),
-        &"down_test.nep".to_string(),
-    )
-    .unwrap();
-}
+// #[test]
+// fn test_download() {
+//     download(
+//         &"http:/localhost:3000/api/redirect?path=/nep/Microsoft/VSCode/VSCode_1.85.1.0_Cno.nep"
+//             .to_string(),
+//         &"down_test.nep".to_string(),
+//     )
+//     .unwrap();
+// }
