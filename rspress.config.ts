eeb6c3a9c5a8cdfabbed22611e247b03ad1e21ca
Cno <dsyourshy@qq.com>
2024-07-21 18:06:46 +0800

    chore: 修复rspress错误

--- rspress.config.ts
+++ rspress.config.ts
@@ -1,32 +1,31 @@
-import * as path from 'path';
-import { defineConfig } from 'rspress/config';
-import { pluginShiki } from '@rspress/plugin-shiki';
+import * as path from "path";
+import { defineConfig } from "rspress/config";
+import { pluginShiki } from "@rspress/plugin-shiki";
 
 export default defineConfig({
-  root: path.join(__dirname, 'doc'),
+  root: path.join(__dirname, "doc"),
   // base:'ept',
-  title:'ept',
-  description:'Modern package solution for Windows',
-  icon:'https://home.edgeless.top/favicon.ico',
+  title: "ept",
+  description: "Modern package solution for Windows",
+  icon: "https://home.edgeless.top/favicon.ico",
   themeConfig: {
-    footer:{
-      message:'MPL-2.0 Licensed | Rendered by Rspress',
+    footer: {
+      message: "MPL-2.0 Licensed | Rendered by Rspress",
     },
     socialLinks: [
       {
-        icon: 'github',
-        mode:'link',
-        content: 'https://github.com/EdgelessPE/ept',
+        icon: "github",
+        mode: "link",
+        content: "https://github.com/EdgelessPE/ept",
       },
     ],
     editLink: {
-      docRepoBaseUrl:
-        'https://github.com/EdgelessPE/ept/edit/develop/doc',
-      text: 'Edit this page on GitHub',
+      docRepoBaseUrl: "https://github.com/EdgelessPE/ept/edit/develop/doc",
+      text: "Edit this page on GitHub",
     },
   },
-  markdown:{
+  markdown: {
     checkDeadLinks: true,
   },
-  plugins:[pluginShiki()]
+  plugins: [pluginShiki()],
 });
