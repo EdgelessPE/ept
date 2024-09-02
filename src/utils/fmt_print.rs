7fadca1d18cb793a5874fb643f963219cf319502
Cno <dsyourshy@qq.com>
2024-09-02 20:48:37 +0800

    fix: 修复wt下black字体看不见

--- src/utils/fmt_print.rs
+++ src/utils/fmt_print.rs
@@ -7,16 +7,16 @@ pub fn fmt_log(head: ColoredString, msg: &str) -> String {
 }
 
 pub fn fmt_log_in_step(step: &str, head: ColoredString, msg: &str) -> String {
-    format!("{head:>8} {s:<9} {msg}", s = step.black())
+    format!("{head:>8} {s:<9} {msg}", s = step.truecolor(100, 100, 100))
 }
 
 pub fn fmt_package_line(scope: &str, name: &str, version: &str, mirror: Option<String>) -> String {
     format!(
         "  {:>20}/{:<50} {:<20} {}\n",
-        scope.black().italic(),
+        scope.truecolor(100, 100, 100).italic(),
         name.cyan().bold(),
         format!("({version})"),
-        mirror.unwrap_or_default().as_str().black()
+        mirror.unwrap_or_default().as_str().truecolor(100, 100, 100)
     )
 }
 
@@ -24,15 +24,18 @@ pub fn fmt_mirror_line(name: &str, updated_at: SystemTime) -> String {
     let date_time: DateTime<chrono::Local> = updated_at.into();
     let time_str = date_time.format("%Y-%m-%d %H:%M:%S").to_string();
     let update_str = format!("updated at {time_str}");
-    format!("  {name:<20} {str}\n", str = update_str.as_str().black())
+    format!(
+        "  {name:<20} {str}\n",
+        str = update_str.as_str().truecolor(100, 100, 100)
+    )
 }
 
 #[test]
 fn test_fmt() {
-    println!("{}", fmt_log("Test".black(), "This is a fmt test message"));
+    println!("{}", fmt_log("Test".purple(), "This is a fmt test message"));
     println!(
         "{}",
-        fmt_log_in_step("Test", "Test".black(), "This is a fmt test message")
+        fmt_log_in_step("Test", "Test".purple(), "This is a fmt test message")
     );
     print!(
         "{}",
