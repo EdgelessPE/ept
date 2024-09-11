5716cb27a01ddf4261561d06b29429e89d072621
Cno <dsyourshy@qq.com>
2024-09-12 01:15:00 +0800

    chore: 对齐插件的国际化

--- rspress.config.ts
+++ rspress.config.ts
@@ -25,12 +25,27 @@ export default defineConfig({
     },
     locales: [
       {
-        lang: "en",
-        label: "ON THIS Page",
+        lang: "zh",
+        label: "简体中文",
+        editLink: {
+          docRepoBaseUrl: "https://github.com/EdgelessPE/ept/edit/develop/doc",
+          text: "在 GitHub 上编辑此页",
+        },
+        prevPageText: "上一篇",
+        nextPageText: "下一篇",
+        outlineTitle: "目录",
+        searchPlaceholderText: "搜索",
+        searchNoResultsText: "未搜索到相关结果",
+        searchSuggestedQueryText: "可更换不同的关键字后重试",
       },
       {
-        lang: "zh",
-        label: "大纲",
+        lang: "en",
+        label: "English",
+        editLink: {
+          docRepoBaseUrl: "https://github.com/EdgelessPE/ept/edit/develop/doc",
+          text: "Edit this page on GitHub",
+        },
+        searchPlaceholderText: "Search",
       },
     ],
   },
