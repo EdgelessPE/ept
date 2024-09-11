a1cb40fc4a1eb9988e6ad600773c4311273ca774
Cno <dsyourshy@qq.com>
2024-09-12 00:35:15 +0800

    chore: add prettier support for eslint v9

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,11 +1,13 @@
 import globals from "globals";
 import pluginJs from "@eslint/js";
-import tseslint from "typescript-eslint";
+import tsEslint from "typescript-eslint";
+import prettier from "eslint-plugin-prettier/recommended";
 
 
 export default [
-  {files: ["**/*.{js,mjs,cjs,ts}"]},
-  {languageOptions: { globals: globals.browser }},
+  { files: ["**/*.{js,mjs,cjs,ts}"] },
+  { languageOptions: { globals: globals.browser } },
   pluginJs.configs.recommended,
-  ...tseslint.configs.recommended,
+  ...tsEslint.configs.recommended,
+  prettier
 ];
