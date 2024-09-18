00541ac6354998baaa94910cf2485d42d7c800e2
Cno <dsyourshy@qq.com>
2024-09-19 00:18:05 +0800

    chore: 统一脚本输出格式

--- scripts/toolchain_utils/upgrade.cmd
+++ scripts/toolchain_utils/upgrade.cmd
@@ -1,10 +1,11 @@
 @echo off
 title ept upgrade
 
-echo Waiting for parent process evacuation...
+echo Info: Waiting for parent process evacuation...
 @ping 127.0.0.1 -n 2 >nul
 
 copy /y release\* "{target}"
-echo Updated successfully
+
+echo Success: Updated successfully
 @ping 127.0.0.1 -n 3 >nul
 exit 0
