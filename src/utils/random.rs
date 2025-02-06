a960c15831f58c0f2aceb9e3da3a8bbb3e05024f
Cno <dsyourshy@qq.com>
2025-02-06 23:59:40 +0800

    chore: update rand

--- src/utils/random.rs
+++ src/utils/random.rs
@@ -1,7 +1,7 @@
-use rand::{distributions::Alphanumeric, Rng};
+use rand::{distr::Alphanumeric, Rng};
 
 pub fn random_short_string() -> String {
-    let s: String = rand::thread_rng()
+    let s: String = rand::rng()
         .sample_iter(&Alphanumeric)
         .take(7)
         .map(char::from)
