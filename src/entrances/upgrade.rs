a8573bf0eb5a5cd6cb570762612388427668c659
Cno <dsyourshy@qq.com>
2025-12-01 00:46:41 +0800

    test: 测试主程序替换成功

--- src/entrances/upgrade.rs
+++ src/entrances/upgrade.rs
@@ -149,11 +149,11 @@ fn test_upgrade() {
 
     // 等待 3s 后断言程序被更新
     sleep(Duration::from_secs(3));
-    assert!(toolchain_path.join("what can i say.man").exists());
     assert_eq!(
         compute_hash_blake3(&p2s!(toolchain_path.join("ept.exe"))).unwrap(),
         "47902cfe5ef75cae1b7cd0497b9b36f98847e55f1afb20d1799f99daf6c40ee4".to_string()
     );
+    assert!(toolchain_path.join("what can i say.man").exists());
 
     // 还原原有的镜像文件夹
     crate::utils::test::_restore_mirror_data(mock_ctx);
