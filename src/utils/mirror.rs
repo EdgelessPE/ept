9ba17831958eb67e8f326da38438c59ca2c04a8c
Cno <dsyourshy@qq.com>
2026-01-29 01:40:54 +0800

    chore: update bincode

--- src/utils/mirror.rs
+++ src/utils/mirror.rs
@@ -223,11 +223,14 @@ pub fn build_index_for_mirror(content: MirrorPkgSoftware, dir: PathBuf) -> Resul
     }
 
     // 写索引
-    let serialized_quick_map = bincode::serialize(&QuickMaps {
-        scope_map,
-        full_map,
-        url_template: content.url_template,
-    })?;
+    let serialized_quick_map = bincode::serde::encode_to_vec(
+        &QuickMaps {
+            scope_map,
+            full_map,
+            url_template: content.url_template,
+        },
+        bincode::config::standard(),
+    )?;
     let quick_path = dir.join(MIRROR_FILE_QUICK_MAP);
     std::fs::write(&quick_path, serialized_quick_map).map_err(|e| {
         anyhow!(
@@ -315,12 +318,13 @@ pub fn read_quick_maps(mirror_name: &str) -> Result<QuickMaps> {
             p2s!(quick_path)
         )
     })?;
-    let quick_map: QuickMaps = bincode::deserialize(&bin_data).map_err(|e| {
-        anyhow!(
-            "Error:Invalid quick map bin at '{}' : {e}",
-            p2s!(quick_path)
-        )
-    })?;
+    let (quick_map, _): (QuickMaps, usize) =
+        bincode::serde::decode_from_slice(&bin_data, bincode::config::standard()).map_err(|e| {
+            anyhow!(
+                "Error:Invalid quick map bin at '{}' : {e}",
+                p2s!(quick_path)
+            )
+        })?;
 
     Ok(quick_map)
 }
